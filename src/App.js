48ed30527cb021d9292a421911041be668d56063
llthompson <lisalacythompson+github@gmail.com>
2023-11-05 13:01:51 -0600

    feed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 
 function App() {
+
   const [articles, setArticles] = useState([]);
   const updateArticles = async (e) => {
     const { data } = await axios.get(`http://hn.algolia.com/api/v1/search?query=${e.target.value}`
     );
     setArticles(data.hits)
   }
+  useEffect(() => {
+    async function fetchArticles() {
+      console.log('fetching')
+      const { data } = await axios.get(`http://hn.algolia.com/api/v1/search`
+      );
+      console.log('setting')
+      setArticles(data.hits)
+    }
+    fetchArticles()
+
+  }, [])
+
   return (
     <div className="App">
-      <form>
-        <input onChange={updateArticles} />
-      </form>
+
+      <header>
+        <form>
+          <input onChange={updateArticles} />
+        </form>
+      </header>
+
 
       <div>
         {articles.map((article, i) => <p key={i}>{article.title}</p>)}
