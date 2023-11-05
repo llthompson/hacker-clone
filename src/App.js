17563b3ec1f4f6a65a6a4732586f83d264730687
llthompson <lisalacythompson+github@gmail.com>
2023-11-05 11:47:18 -0600

    initial api connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const updateArticles = async (e) => {
+    const { data } = await axios.get(`http://hn.algolia.com/api/v1/search?query=${e.target.value}`
+    );
+    setArticles(data.hits)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input onChange={updateArticles} />
+      </form>
+
+      <div>
+        {articles.map((article, i) => <p key={i}>{article.title}</p>)}
+      </div>
     </div>
   );
 }
